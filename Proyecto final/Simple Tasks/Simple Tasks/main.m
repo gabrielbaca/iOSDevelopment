82fbacd75e18d5c274ad79dee10cf1d85b6ef96e
gabrielbaca <alexx342@gmail.com>
2014-03-23 23:29:49 -0600

    Initial iOS project development

---
+++ Proyecto final/Simple Tasks/Simple Tasks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Simple Tasks
+//
+//  Created by Alejandro Baca on 3/23/14.
+//  Copyright (c) 2014 Gabriel Baca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
